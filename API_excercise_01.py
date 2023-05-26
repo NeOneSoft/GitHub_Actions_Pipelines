7fc682f482c58dab1bee01a65a888342d09e2f5b
Gonzalo Alfredo Romero Hernandez <gonclapton@hotmail.com>
2023-05-26 11:05:43 -0600

    feat: add and API and its unittest

---
+++ API_excercise_01.py
@@ -0,0 +1,79 @@
+from flask import Flask, jsonify, request
+import sqlite3
+
+app = Flask(__name__)
+
+# Endpoint para obtener información de un usuario por su ID
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    # Establecer conexión con la base de datos
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Obtener información del usuario desde la base de datos
+    cursor.execute('SELECT * FROM users WHERE id = ?', (user_id,))
+    user = cursor.fetchone()
+
+    # Cerrar la conexión con la base de datos
+    cursor.close()
+    conn.close()
+
+    if user is None:
+        return jsonify({'message': 'Usuario no encontrado'}), 404
+
+    # Convertir la información del usuario en un diccionario
+    user_data = {
+        'id': user[0],
+        'name': user[1],
+        'email': user[2]
+    }
+
+    return jsonify(user_data)
+
+# Endpoint para crear un nuevo usuario
+@app.route('/users', methods=['POST'])
+def create_user():
+    # Obtener los datos del nuevo usuario desde el cuerpo de la solicitud
+    data = request.get_json()
+    name = data['name']
+    email = data['email']
+
+    # Establecer conexión con la base de datos
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Insertar los datos del nuevo usuario en la base de datos
+    cursor.execute('INSERT INTO users (name, email) VALUES (?, ?)', (name, email))
+    conn.commit()
+
+    # Obtener el ID del nuevo usuario
+    user_id = cursor.lastrowid
+
+    # Cerrar la conexión con la base de datos
+    cursor.close()
+    conn.close()
+
+    # Devolver una respuesta exitosa con el ID del nuevo usuario
+    return jsonify({'message': 'Usuario creado exitosamente', 'id': user_id})
+
+if __name__ == '__main__':
+    # Crear la tabla de usuarios en la base de datos (solo para propósitos de ejemplo)
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT)')
+    cursor.close()
+    conn.close()
+
+    # Ejecutar la aplicación Flask
+    app.run()
+
+'''
+Curl:
+curl -X POST -H "Content-Type: application/json" -d '{
+  "name": "Alfredo Romero",
+  "email": "alfredoromero@example.com"
+}' http://localhost:5000/users
+
+curl -X GET http://localhost:5000/users/1
+
+'''
---
+++ API_excercise_01_pytest.py
@@ -0,0 +1,36 @@
+import pytest
+import json
+from API_excercise_01 import app
+
+def test_get_user():
+    client = app.test_client()
+    response = client.get('/users/1')
+    data = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 200
+    assert data['id'] == 1
+    assert data['name'] == 'John Doe'
+    assert data['email'] == 'johndoe@example.com'
+
+def test_get_user_not_found():
+    client = app.test_client()
+    response = client.get('/users/999')
+    data = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 404
+    assert data['message'] == 'Usuario no encontrado'
+
+def test_create_user():
+    client = app.test_client()
+    data = {
+        'name': 'Jane Smith',
+        'email': 'janesmith@example.com'
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = client.post('/users', headers=headers, data=json.dumps(data))
+    data = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 200
+    assert data['message'] == 'Usuario creado exitosamente'
+    assert data['id'] is not None
+
